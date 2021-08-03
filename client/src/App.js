d485b370a69f7706729bca7c236559a78f2a747e
Ashar Farooq <ashar744@gmail.com>
2021-08-02 20:24:41 -0400

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <label for="name">Name:</label>
+      <input id="name" type="text" />
+
+      <label for="age">Age:</label>
+      <input id="age" type="number" />
+
+      <label for="country">Country:</label>
+      <input id="country" type="text" />
+
+      <label for="role">Role:</label>
+      <input id="role" type="text" />
+
+      <label for="wage">Wage:</label>
+      <input id="wage" type="number" />
+
+    </div>
+  );
+}
+
+export default App;
